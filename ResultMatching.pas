2ef18b7b78710c74f30251b749c32314917d46ed
suporte-dev <alexsystem@gmail.com>
2024-09-27 21:46:18 -0300

    Update ResultMatching.pas
    Add método com procedure anônima(MatchProcedures)

--- ResultMatching.pas
+++ ResultMatching.pas
@@ -3,36 +3,34 @@ unit ResultMatching;
 interface
 
 uses
-  System.Classes, System.Generics.Collections, System.SysUtils;
+  System.SysUtils, System.Generics.Collections;
 
 type
-  // Classe que encapsula uma mensagem de erro simples
-  TError = class
-  private
-    FMessage: string;
-  public
-    constructor Create(AMessage: string);
-    property Message: string read FMessage;
-  end;
-
-  // Classe que encapsula detalhes mais especÌficos sobre um erro
-  TErrResult = class
+  // Record que encapsula detalhes mais espec√≠ficos sobre um erro
+  TErrResult = record
   private
     FCode: Integer;
     FDescription: string;
   public
+    // Construtor para inicializar o record
     constructor Create(ACode: Integer; ADescription: string);
+
+    // Operador implicit para facilitar a cria√ß√£o de erros
+    class operator Implicit(AError: string): TErrResult;
+    class operator Implicit(ACode: Integer): TErrResult;
+
     property Code: Integer read FCode;
     property Description: string read FDescription;
   end;
 
-  // Record genÈrico para armazenar o resultado do Match
-  TMatchResult<U> = record
+  // Record gen√©rico para armazenar o resultado do Match
+  TMatchResult<U, E> = record
     IsOk: Boolean;
-    Value: U;
+    OkValue: U;
+    ErrValue: E;
   end;
 
-  // Record genÈrico que representa uma operaÁ„o que pode ser bem-sucedida ou falhar
+  // Record gen√©rico que representa uma opera√ß√£o que pode ser bem-sucedida ou falhar
   TResultOptions<T> = record
   private
     FValue: T;
@@ -41,101 +39,127 @@ type
     procedure InitOk(AValue: T);
     procedure InitErr(AError: TErrResult);
   public
-    // Operadores implÌcitos para facilitar a criaÁ„o de TResultOptions
+    // Operadores impl√≠citos para facilitar a cria√ß√£o de TResultOptions
     class operator Implicit(AValue: T): TResultOptions<T>;
     class operator Implicit(AError: TErrResult): TResultOptions<T>;
 
-    // MÈtodos para verificar o estado da operaÁ„o
+    // M√©todos para verificar o estado da opera√ß√£o
     function IsOk: Boolean;
     function IsErr: Boolean;
 
-    // MÈtodos para acessar o valor ou o erro
+    // M√©todos para acessar o valor ou o erro
     function Value: T;
     function Error: TErrResult;
 
-    // MÈtodo Match para implementar o Pattern Matching
-    function Match<U>(OnOk: TFunc<T, U>; OnErr: TFunc<TErrResult, U>): TMatchResult<U>;
+    // M√©todo Match para functions (Pattern Matching com retorno)
+    function Match<U, E>(OnOk: TFunc<T, U>; OnErr: TFunc<TErrResult, E>): TMatchResult<U, E>;
+
+    // M√©todo Match para procedures an√¥nimas (Pattern Matching sem retorno)
+    procedure MatchProcedures(OnOk: TProc<T>; OnErr: TProc<TErrResult>);
   end;
 
 implementation
 
-{ TError }
-
-constructor TError.Create(AMessage: string);
-begin
-  FMessage := AMessage;
-end;
-
 { TErrResult }
 
+// Construtor para inicializar o record
 constructor TErrResult.Create(ACode: Integer; ADescription: string);
 begin
   FCode := ACode;
   FDescription := ADescription;
 end;
 
+// Operador implicit para converter uma string em TErrResult
+class operator TErrResult.Implicit(AError: string): TErrResult;
+begin
+  Result := TErrResult.Create(0, AError);  // Erro gen√©rico com c√≥digo 0
+end;
+
+// Operador implicit para converter um integer em TErrResult
+class operator TErrResult.Implicit(ACode: Integer): TErrResult;
+begin
+  Result := TErrResult.Create(ACode, 'Erro desconhecido');
+end;
+
 { TResultOptions<T> }
 
+// Inicializa com sucesso
 procedure TResultOptions<T>.InitOk(AValue: T);
 begin
   FValue := AValue;
   FIsOk := True;
-  FError := nil;
+  FError := '';  // Sem erro
 end;
 
+// Inicializa com erro
 procedure TResultOptions<T>.InitErr(AError: TErrResult);
 begin
   FIsOk := False;
   FError := AError;
 end;
 
+// Operador implicit para converter um valor gen√©rico em TResultOptions
 class operator TResultOptions<T>.Implicit(AValue: T): TResultOptions<T>;
 begin
   Result.InitOk(AValue);
 end;
 
+// Operador implicit para converter TErrResult em TResultOptions
 class operator TResultOptions<T>.Implicit(AError: TErrResult): TResultOptions<T>;
 begin
   Result.InitErr(AError);
 end;
 
+// Verifica se o resultado foi um sucesso
 function TResultOptions<T>.IsOk: Boolean;
 begin
   Result := FIsOk;
 end;
 
+// Verifica se o resultado foi um erro
 function TResultOptions<T>.IsErr: Boolean;
 begin
   Result := not FIsOk;
 end;
 
+// Retorna o valor em caso de sucesso, ou levanta uma exce√ß√£o se houver erro
 function TResultOptions<T>.Value: T;
 begin
   if not FIsOk then
-    raise Exception.Create('Nenhum valor disponÌvel. A operaÁ„o resultou em erro.');
+    raise Exception.Create('Nenhum valor dispon√≠vel. A opera√ß√£o resultou em erro.');
   Result := FValue;
 end;
 
+// Retorna o erro em caso de falha, ou levanta uma exce√ß√£o se houve sucesso
 function TResultOptions<T>.Error: TErrResult;
 begin
   if FIsOk then
-    raise Exception.Create('Nenhum erro. A operaÁ„o foi bem-sucedida.');
+    raise Exception.Create('Nenhum erro. A opera√ß√£o foi bem-sucedida.');
   Result := FError;
 end;
 
-function TResultOptions<T>.Match<U>(OnOk: TFunc<T, U>; OnErr: TFunc<TErrResult, U>): TMatchResult<U>;
+// M√©todo Match com functions (Pattern Matching com retorno de valor)
+function TResultOptions<T>.Match<U, E>(OnOk: TFunc<T, U>; OnErr: TFunc<TErrResult, E>): TMatchResult<U, E>;
 var
-  matchResult: TMatchResult<U>;
+  matchResult: TMatchResult<U, E>;
 begin
   matchResult.IsOk := FIsOk;
 
   if FIsOk then
-    matchResult.Value := OnOk(FValue)  // Passamos FValue para a funÁ„o OnOk
+    matchResult.OkValue := OnOk(FValue)  // Executa OnOk e atribui o retorno a OkValue
   else
-    matchResult.Value := OnErr(FError); // Passamos FError para a funÁ„o OnErr
+    matchResult.ErrValue := OnErr(FError); // Executa OnErr e atribui o retorno a ErrValue
 
   Result := matchResult;
 end;
 
-end.
+// M√©todo Match com procedures an√¥nimas (Pattern Matching sem retorno de valor)
+procedure TResultOptions<T>.MatchProcedures(OnOk: TProc<T>; OnErr: TProc<TErrResult>);
+begin
+  if FIsOk then
+    OnOk(FValue)  // Executa a procedure para o caso de sucesso
+  else
+    OnErr(FError); // Executa a procedure para o caso de erro
+end;
 
+end.
